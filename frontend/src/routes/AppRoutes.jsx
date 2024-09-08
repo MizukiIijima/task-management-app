1c6e7c042f81a768d1f8c444986ee37b1893e719
mizuki-iijima <zima.web05@gmail.com>
2024-09-08 15:15:39 +0900

    frontend ルーティングの修正

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,11 +2,13 @@ import { Route, createBrowserRouter, createRoutesFromElements } from "react-rout
 import { TaskRegister } from "../components/TaskRegister";
 import { Login } from "../components/Login";
 import { ProjectRegister } from "../components/ProjectRegister.jsx";
+import { TopPage } from "../components/TopPage";
 import App from  "../App.jsx";
 
 export const AppRoutes = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />} >
+            <Route index element={<TopPage />} />
             <Route path="task-register" element={<TaskRegister />} />
             <Route path="project-register" element={<ProjectRegister />} />
             <Route path="login" element={<Login />} />
