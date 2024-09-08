4f224c4e40c4f763f065e637d6972153cb5605e2
mizuki-iijima <zima.web05@gmail.com>
2024-09-08 23:08:03 +0900

    frontend プロジェクトを登録する処理を追加

--- frontend/src/components/ProjectRegister.jsx
+++ frontend/src/components/ProjectRegister.jsx
@@ -1,13 +1,32 @@
 import { useForm } from "react-hook-form";
 import { TextField, Button } from "@mui/material";
 import "./ProjectRegister.css";
+import { useEffect, useState } from "react";
 
 export const ProjectRegister = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, getValues } = useForm();
+    const [message, setMessage] = useState('');
+    
+    const projectSubmit = async () => {
+        // setProjectName(getValues('project_name'));
+        const project_name = getValues('project_name');
+        const project_detail = getValues('project_detail');
 
-    const projectSubmit = () => {
-        alert('OK')
+        const response = await fetch('/projects', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ project_name, project_detail }),
+        });
+
+        const result = (await response).json();
+        if(response.ok){
+            setMessage('プロジェクトの登録が完了しました。');
+        } else {
+            setMessage('プロジェクトの登録に失敗しました。');
+        }
     }
 
     return(
@@ -18,6 +37,9 @@ export const ProjectRegister = () => {
                     label="プロジェクト名"
                     fullWidth
                     sx={{ display: "block", marginBottom: "1.875rem", }}
+                    {...register('project_name',{
+                        required: '必須項目です。'
+                    })}
                 />
                 <TextField
                     label="タスク内容"
@@ -25,11 +47,13 @@ export const ProjectRegister = () => {
                     rows={10}
                     fullWidth
                     sx={{ display: "block", marginBottom: "4.125rem" }}
+                    {...register('project_detail')}
                 />
                 <Button variant="contained" type="submit"
                     sx={{ backgroundColor: "#37C300", width: "12rem", margin: "auto", display: "block" }}
                 >プロジェクトを作成</Button>
             </form>
+            {message && <p>{message}</p>}
         </div>
     )
 
