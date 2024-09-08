5b16e96321afc2af6d1710a5d70b2950b941d17a
mizuki-iijima <zima.web05@gmail.com>
2024-09-08 15:34:11 +0900

    frontend TopPageコンポーネントにボタンを追加

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+import { ProjectRegister } from "./ProjectRegister";
 import "./TopPage.css";
 
 export const TopPage = () => {
 
     return(
-        <p className="message">まずはプロジェクトを作成しましょう！</p>
+        <div className="topPage">
+            <p className="message">まずはプロジェクトを作成しましょう！</p>
+            <Link to="/project-register" className="link-button">
+                プロジェクト作成
+            </Link>
+        </div>
+
     )
 }
