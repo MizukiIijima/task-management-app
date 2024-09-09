ed394a979ca26a6c9c0bf71ccffb8a7ba6717bf6
mizuki-iijima <zima.web05@gmail.com>
2024-09-09 14:54:30 +0900

    frontend プロジェクトを取得するコードを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,43 @@
 import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
-// import { AppRoutes } from './routes/AppRoutes.jsx';
-
 function App() {
-    const [isLogin, setIsLogin] = useState(false);
+
+    //ID、プロジェクト名、プロジェクト詳細
+    const [projects, setProjects] = useState();
+
+    useEffect(() => {
+        const fetchProjectsName = async () => {
+            try {
+                const response = await fetch('/projects');
+                if(response.ok){
+                    const data = await response.json();
+                    setProjects(data);
+                } else {
+                    console.error('プロジェクト名の取得に失敗しました。')
+                }    
+            } catch {
+                console.error('エラーが発生しました。');
+            }
+        }
+
+        fetchProjectsName();
+    }, []);
+
+    
+    useEffect(() => {
+        console.log(projects)
+    }, [projects]);
 
     return (
         <>
             <Header />
-            {/* <ul>
-                <li><Link to="/login">ログイン画面</Link></li>
-                <li><Link to="/project-register">プロジェクト作成</Link></li>
-                <li><Link to="/task-register">タスク登録</Link></li>
-            </ul> */}
             <div className="wrapper">
-                <Sidebar />
-                <Outlet />
+                <Sidebar projects={projects}/>
+                <Outlet context={{projects, setProjects}}/>
             </div>
         </>
     );
