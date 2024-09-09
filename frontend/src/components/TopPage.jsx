714e51154ff8c231ad4ab0f5c2e07e0259f5c8ae
mizuki-iijima <zima.web05@gmail.com>
2024-09-09 22:46:15 +0900

    frontend TopPageにプロジェクト一覧を表示

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,16 +1,32 @@
-import { Link } from "react-router-dom";
-import { ProjectRegister } from "./ProjectRegister";
+import { Link, useOutletContext } from "react-router-dom";
+// import { ProjectRegister } from "./ProjectRegister";
 import "./TopPage.css";
+import { useEffect } from "react";
 
 export const TopPage = () => {
 
-    return(
+    const { projects, setProjects } = useOutletContext();
+    
+    return (
         <div className="topPage">
-            <p className="message">まずはプロジェクトを作成しましょう！</p>
-            <Link to="/projects/register" className="link-button">
-                プロジェクト作成
-            </Link>
+            {Object.keys(projects).length === 0 ? 
+                <>
+                {/* 1件もプロジェクトがない場合の表示 */}
+                    <p className="message">まずはプロジェクトを作成しましょう！</p>
+                    <Link to="/projects/register" className="link-button">
+                        プロジェクトを作成
+                    </Link>
+                </> : 
+                // プロジェクトがある場合
+                <>
+                    <h1>プロジェクト一覧</h1>
+                    <div className="topPageProject">
+                        {projects.map((project, index) => (
+                            <h2 key={index}>{project.project_name}</h2>
+                        ))}
+                    </div>
+                </>
+            }
         </div>
-
     )
 }
