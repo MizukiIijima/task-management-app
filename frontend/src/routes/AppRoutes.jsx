a047c18d4b3b7a0ec4dae44d8d3bd3f4806ab135
mizuki-iijima <zima.web05@gmail.com>
2024-09-11 12:16:38 +0900

    frontend タスク登録画面へのルーティングを修正

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,12 +4,14 @@ import { Login } from "../components/Login";
 import { ProjectRegister } from "../components/ProjectRegister.jsx";
 import { TopPage } from "../components/TopPage";
 import App from  "../App.jsx";
+import { ProjectDetail } from "../components/ProjectDetail.jsx";
 
 export const AppRoutes = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />} >
             <Route index element={<TopPage />} />
-            <Route path="task-register" element={<TaskRegister />} />
+            <Route path="task/register/:id" element={<TaskRegister />} />
+            <Route path="projects/detail/:id" element={<ProjectDetail />} />
             <Route path="projects/register" element={<ProjectRegister />} />
             <Route path="login" element={<Login />} />
         </Route>
