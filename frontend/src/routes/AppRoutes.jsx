c97550d0cccf53de620bca7542ade73b7692986c
mizuki-iijima <zima.web05@gmail.com>
2024-09-12 22:56:29 +0900

    frontend ルーティング追加

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { TaskRegister } from "../components/TaskRegister";
 import { Login } from "../components/Login";
 import { ProjectRegister } from "../components/ProjectRegister.jsx";
 import { TopPage } from "../components/TopPage";
+import { TaskDetail } from "../components/TaskDetail.jsx";
 import App from  "../App.jsx";
 import { ProjectDetail } from "../components/ProjectDetail.jsx";
 
@@ -10,7 +11,8 @@ export const AppRoutes = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />} >
             <Route index element={<TopPage />} />
-            <Route path="task/register/:id" element={<TaskRegister />} />
+            <Route path="tasks/register/:id" element={<TaskRegister />} />
+            <Route path="tasks/detail/:id"   element={<TaskDetail />} />
             <Route path="projects/detail/:id" element={<ProjectDetail />} />
             <Route path="projects/register" element={<ProjectRegister />} />
             <Route path="login" element={<Login />} />
