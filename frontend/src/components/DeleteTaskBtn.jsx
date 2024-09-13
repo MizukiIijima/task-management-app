379388d2d8920a164f4e2a31b836a46cf68191f3
mizuki-iijima <zima.web05@gmail.com>
2024-09-13 16:09:45 +0900

    frontend タスク削除ボタン追加

---
+++ frontend/src/components/DeleteTaskBtn.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material";
+
+export const DeleteTaskBtn = () => {
+
+    const taskCreate = () => {
+
+    }
+
+    return(
+        <Button
+            variant="contained"
+            color="error"
+            sx={{ width: "12rem", lineHeight: "2.25rem", margin: '5rem auto 0 auto', display: 'block' }}
+            onClick={taskCreate}>
+            タスクを削除する
+        </Button>
+    );
+}
