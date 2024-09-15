5241b1dc8a47fe1268592724b419f9a23de7472b
mizuki-iijima <zima.web05@gmail.com>
2024-09-15 17:38:22 +0900

    backend タスク修正用のコードを追加

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,11 +37,10 @@ app.post('/api/projects', (req, res) => {
     });
 });
 
-//タスクの取得
+//プロジェクト詳細画面で全タスクの取得
 app.get('/api/projects/detail/:id', (req, res) => {
     
     const projectId = req.params.id;
-    console.log(projectId)
     const getTasksQuery = `SELECT * FROM tasks WHERE project_id = ?`;
 
     db.all(getTasksQuery, [projectId], (err, rows) => {
@@ -50,9 +49,25 @@ app.get('/api/projects/detail/:id', (req, res) => {
             console.error(err);
         } else {
             res.status(200).json({ tasks: rows });
-            console.log(rows)
         }
     });
+});
+
+//タスク詳細画面でのタスク取得
+app.get('/api/tasks/detail/:id', (req, res) => {
+
+    const taskId = req.params.id;
+    const searchTasksQuery = 'SELECT * FROM tasks WHERE id = ?';
+
+    db.all(searchTasksQuery, [taskId], (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: 'データベースエラーが発生しました。'});
+            console.error(err);
+        } else {
+            res.status(200).json({ tasks: rows });
+            console.log(rows)
+        }
+    })
 });
 
 //タスク登録
@@ -67,6 +82,51 @@ app.post('/api/tasks', (req, res) => {
             console.error(err);
         } else {
             res.status(201).json({ message: '正常に登録されました。' });
+        }
+    });
+});
+
+//タスク修正
+app.post('/api/tasks/update/:id', (req, res) => {
+
+    const { project_id, task_name, content, person, status, progress, date } = req.body;
+
+    //修正するタスクID取得
+    const editTaskId = req.params.id;
+
+    const taskEditQuery = `
+    UPDATE tasks 
+    SET 
+        task_name = CASE WHEN task_name != ? THEN ? ELSE task_name END,
+        content = CASE WHEN content != ? THEN ? ELSE content END,
+        person = CASE WHEN person != ? THEN ? ELSE person END,
+        status = CASE WHEN status != ? THEN ? ELSE status END,
+        progress = CASE WHEN progress != ? THEN ? ELSE progress END,
+        date = CASE WHEN date != ? THEN ? ELSE date END
+    WHERE id = ${editTaskId}`;
+
+    const params = [
+        task_name, task_name,
+        content, content,
+        person, person,
+        status, status,
+        progress, progress,
+        date, date,
+    ];
+
+    db.run(taskEditQuery, params, function (err) {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ message: '失敗しました。' });
+        } else {
+            db.get(`SELECT * FROM tasks WHERE id = ${editTaskId}`, (err, rows, updatedTask) => {
+                if(err) {
+                    res.status(500).json({ error: 'エラーが発生' });
+                } else {
+                    console.log(rows);
+                    res.status(200).json(updatedTask);
+                }
+            });
         }
     });
 });
